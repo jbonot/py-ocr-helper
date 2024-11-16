55ad849b3e964d8bb17c3149d40489225475a6ce
Jeanine Bonot <jeaninebonot@gmail.com>
2024-11-16 13:28:17 +0100

    test: add integration test

---
+++ tests/test_ocrhelper.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import ocrhelper
+
+input_dir = os.path.join(os.path.dirname(__file__), "data", "input")
+processed_dir = os.path.join(os.path.dirname(__file__), "data", "processed")
+output_dir = os.path.join(os.path.dirname(__file__), "data", "output")
+
+# Create output directories if they don't exist
+os.makedirs(processed_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
+
+for file_name in os.listdir(input_dir):
+    if file_name.lower().endswith((".bmp", ".png", ".jpg", ".jpeg")):
+        input_path = os.path.join(input_dir, file_name)
+        processed_path = os.path.join(processed_dir, f"processed_{file_name}")
+        output_hocr_path = os.path.join(
+            output_dir, f"{os.path.splitext(file_name)[0]}.hocr"
+        )
+
+        binary_image = ocrhelper.process_image(input_path, processed_path)
+
+        # Save processed image
+        ocrhelper.save_processed_image(processed_path, binary_image)
+
+        extracted_hocr = ocrhelper.extract_text_from_image(
+            binary_image,
+            tesseract_params={
+                "lang": "nld+fra",
+                "config": r"--psm 6",
+                "extension": "hocr",
+            },
+        )
+
+        # Save HOCR to a file
+        with open(output_hocr_path, "w", encoding="utf-8") as hocr_file:
+            hocr_file.write(extracted_hocr)
