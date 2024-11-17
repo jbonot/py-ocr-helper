4d6d2e8219d38fdf8141b1f327a5f002b7439373
Jeanine Bonot <jeaninebonot@gmail.com>
2024-11-17 19:50:47 +0100

    refactor: reorder functions

--- ocrhelper/__init__.py
+++ ocrhelper/__init__.py
@@ -8,11 +8,6 @@ import re
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
-def process_image_from_path(image_path, resize_factor=1):
-    img = Image.open(image_path)
-    return process_image(img, resize_factor)
-
-
 def process_image(image, resize_factor=1):
     processed_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2GRAY)
@@ -29,6 +24,11 @@ def process_image(image, resize_factor=1):
     return binary
 
 
+def process_image_from_path(image_path, resize_factor=1):
+    img = Image.open(image_path)
+    return process_image(img, resize_factor)
+
+
 def save_processed_image(processed_image_path, binary):
     cv2.imwrite(processed_image_path, binary)
 
@@ -40,18 +40,18 @@ def get_hocr_from_image(image, tesseract_params={}):
     return hocr_bytes.decode("utf-8")
 
 
-def get_hocr_from_bbox(bbox, should_process=False, tesseract_params={}):
-    img = ImageGrab.grab(bbox)
-    if should_process:
-        img = process_image(img)
-    return get_hocr_from_image(img, tesseract_params)
-
-
 def read_text_at_position(bbox, should_process=False, tesseract_params={}):
     img = ImageGrab.grab(bbox)
     if should_process:
         img = process_image(img)
     return pytesseract.image_to_string(img, **tesseract_params)
+
+
+def get_hocr_from_bbox(bbox, should_process=False, tesseract_params={}):
+    img = ImageGrab.grab(bbox)
+    if should_process:
+        img = process_image(img)
+    return get_hocr_from_image(img, tesseract_params)
 
 
 def locate_text_at_position(
